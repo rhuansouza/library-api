127019933c670cf130cb2d09543d274f9191f492
rhuancostanzo <rhuancostanzo@hotmail.com>
2021-07-28 14:55:52 -0300

    Retirado Email

--- src/main/java/com/costanzo/libraryapi/LibraryApiApplication.java
+++ src/main/java/com/costanzo/libraryapi/LibraryApiApplication.java
@@ -16,25 +16,11 @@ import java.util.List;
 @EnableScheduling
 public class LibraryApiApplication{
 
-
-	@Autowired
-	private EmailService emailService;
-
-
 	@Bean
 	public ModelMapper modelMapper(){
 		return new ModelMapper();
 	}
 
-	//Executa assim que subir a aplicação
-	/*@Bean
-	public CommandLineRunner runner(){
-		return args -> {
-			List<String> emails = Arrays.asList("library-api-ede181@inbox.mailtrap.io");
-			emailService.sendMails("Testando serviço de emails", emails);
-			System.out.println("EMAILS ENVIADOS");
-		};
-	}*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApiApplication.class, args);
